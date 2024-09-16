7c81139ded078dc9965949b25f2a601fd19d7250
Triet Huynh <triet403@gmail.com>
2024-09-16 11:39:30 -0400

    pre-transitioning to a standalone lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,101 @@
 use geoip_h::GeoIPDBTypes;
-use lazy_static::lazy_static;
+use std::{cell::OnceCell, env};
 mod geoip_h;
-
-use std::{sync::Mutex, usize};
 
 const NUM_DB_TYPES: usize = 33 + 1;
 
-lazy_static! {
-    static ref CUSTOM_DIRECTORY: Mutex<Option<String>> = Mutex::new(None);
-    static ref GEOIP_SETUP_FILENAME: Mutex<Option<[String; NUM_DB_TYPES]>> = Mutex::new(None);
-}
+const GEOIP_DB_FILE_NAME: OnceCell<[String; NUM_DB_TYPES]> = OnceCell::new();
+const CUSTOM_DIRECTORY: OnceCell<String> = OnceCell::new();
 
 pub fn geoip_setup_custom_directory(dir: String) {
-    let mut custom_directory = CUSTOM_DIRECTORY.lock().unwrap();
-    *custom_directory = Some(dir);
+    CUSTOM_DIRECTORY.get_or_init(|| dir);
+}
+
+fn _GeoIP_full_path_to(file_name: &str) -> String {
+    let path;
+    if let Some(custom_dir) = CUSTOM_DIRECTORY.get() {
+        if custom_dir.chars().last().unwrap() != '/' {
+            path = format!("{}/{}", custom_dir, file_name);
+        } else {
+            path = format!("{}{}", custom_dir, file_name);
+        }
+    } else {
+        let geoip_data_dir = env::var("GEOIPDATADIR").unwrap_or_else(|_| "~/GeoIP".to_string());
+
+        path = format!("{}/{}", geoip_data_dir, file_name);
+    }
+    path
 }
 
 pub fn _geo_ip_setup_dbfilename() {
-    let geo_ipdbfile_name = GEOIP_SETUP_FILENAME.lock().unwrap().take();
-    if geo_ipdbfile_name.is_none() {
-        geo_ipdbfile_name.unwrap()[GeoIPDBTypes::GEOIP_COUNTRY_EDITION as usize] =
-            "das".to_string();
-    }
+    GEOIP_DB_FILE_NAME.get_or_init(|| {
+        const EMPTY_STRING: String = String::new();
+        let mut result: [String; NUM_DB_TYPES] = [EMPTY_STRING; NUM_DB_TYPES];
+
+        result[GeoIPDBTypes::GEOIP_COUNTRY_EDITION as usize] = _GeoIP_full_path_to("GeoIP.dat");
+        result[GeoIPDBTypes::GEOIP_REGION_EDITION_REV0 as usize] =
+            _GeoIP_full_path_to("GeoIPRegion.dat");
+        result[GeoIPDBTypes::GEOIP_REGION_EDITION_REV1 as usize] =
+            _GeoIP_full_path_to("GeoIPRegion.dat");
+        result[GeoIPDBTypes::GEOIP_CITY_EDITION_REV0 as usize] =
+            _GeoIP_full_path_to("GeoIPCity.dat");
+        result[GeoIPDBTypes::GEOIP_CITY_EDITION_REV1 as usize] =
+            _GeoIP_full_path_to("GeoIPCity.dat");
+        result[GeoIPDBTypes::GEOIP_ISP_EDITION as usize] = _GeoIP_full_path_to("GeoIPISP.dat");
+        result[GeoIPDBTypes::GEOIP_ORG_EDITION as usize] = _GeoIP_full_path_to("GeoIPOrg.dat");
+        result[GeoIPDBTypes::GEOIP_PROXY_EDITION as usize] = _GeoIP_full_path_to("GeoIPProxy.dat");
+        result[GeoIPDBTypes::GEOIP_ASNUM_EDITION as usize] = _GeoIP_full_path_to("GeoIPASNum.dat");
+        result[GeoIPDBTypes::GEOIP_NETSPEED_EDITION as usize] =
+            _GeoIP_full_path_to("GeoIPNetSpeed.dat");
+        result[GeoIPDBTypes::GEOIP_DOMAIN_EDITION as usize] =
+            _GeoIP_full_path_to("GeoIPDomain.dat");
+        result[GeoIPDBTypes::GEOIP_COUNTRY_EDITION_V6 as usize] =
+            _GeoIP_full_path_to("GeoIPv6.dat");
+        result[GeoIPDBTypes::GEOIP_LOCATIONA_EDITION as usize] =
+            _GeoIP_full_path_to("GeoIPLocA.dat");
+        result[GeoIPDBTypes::GEOIP_ACCURACYRADIUS_EDITION as usize] =
+            _GeoIP_full_path_to("GeoIPDistance.dat");
+        result[GeoIPDBTypes::GEOIP_LARGE_COUNTRY_EDITION as usize] =
+            _GeoIP_full_path_to("GeoIP.dat");
+        result[GeoIPDBTypes::GEOIP_LARGE_COUNTRY_EDITION_V6 as usize] =
+            _GeoIP_full_path_to("GeoIPv6.dat");
+        result[GeoIPDBTypes::GEOIP_ASNUM_EDITION_V6 as usize] =
+            _GeoIP_full_path_to("GeoIPASNumv6.dat");
+        result[GeoIPDBTypes::GEOIP_ISP_EDITION_V6 as usize] = _GeoIP_full_path_to("GeoIPISPv6.dat");
+        result[GeoIPDBTypes::GEOIP_ORG_EDITION_V6 as usize] = _GeoIP_full_path_to("GeoIPOrgv6.dat");
+        result[GeoIPDBTypes::GEOIP_DOMAIN_EDITION_V6 as usize] =
+            _GeoIP_full_path_to("GeoIPDomainv6.dat");
+        result[GeoIPDBTypes::GEOIP_LOCATIONA_EDITION_V6 as usize] =
+            _GeoIP_full_path_to("GeoIPLocAv6.dat");
+        result[GeoIPDBTypes::GEOIP_REGISTRAR_EDITION as usize] =
+            _GeoIP_full_path_to("GeoIPRegistrar.dat");
+        result[GeoIPDBTypes::GEOIP_REGISTRAR_EDITION_V6 as usize] =
+            _GeoIP_full_path_to("GeoIPRegistrarv6.dat");
+        result[GeoIPDBTypes::GEOIP_USERTYPE_EDITION as usize] =
+            _GeoIP_full_path_to("GeoIPUserType.dat");
+        result[GeoIPDBTypes::GEOIP_USERTYPE_EDITION_V6 as usize] =
+            _GeoIP_full_path_to("GeoIPUserTypev6.dat");
+        result[GeoIPDBTypes::GEOIP_CITY_EDITION_REV0_V6 as usize] =
+            _GeoIP_full_path_to("GeoIPCityv6.dat");
+        result[GeoIPDBTypes::GEOIP_CITY_EDITION_REV1_V6 as usize] =
+            _GeoIP_full_path_to("GeoIPCityv6.dat");
+        result[GeoIPDBTypes::GEOIP_NETSPEED_EDITION_REV1 as usize] =
+            _GeoIP_full_path_to("GeoIPNetSpeedCell.dat");
+        result[GeoIPDBTypes::GEOIP_NETSPEED_EDITION_REV1_V6 as usize] =
+            _GeoIP_full_path_to("GeoIPNetSpeedCellv6.dat");
+        result[GeoIPDBTypes::GEOIP_COUNTRYCONF_EDITION as usize] =
+            _GeoIP_full_path_to("GeoIPCountryConf.dat");
+        result[GeoIPDBTypes::GEOIP_CITYCONF_EDITION as usize] =
+            _GeoIP_full_path_to("GeoIPCityConf.dat");
+        result[GeoIPDBTypes::GEOIP_REGIONCONF_EDITION as usize] =
+            _GeoIP_full_path_to("GeoIPRegionConf.dat");
+        result[GeoIPDBTypes::GEOIP_POSTALCONF_EDITION as usize] =
+            _GeoIP_full_path_to("GeoIPPostalConf.dat");
+        result[GeoIPDBTypes::GEOIP_ACCURACYRADIUS_EDITION_V6 as usize] =
+            _GeoIP_full_path_to("GeoIPDistancev6.dat");
+
+        result
+    });
 }
+
+pub fn GeoIP_open( )
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use lazy_static::lazy_static;
-use lib_geoip_rust::geoip_setup_custom_directory;
+use lib_geoip_rust::{_geo_ip_setup_dbfilename, geoip_setup_custom_directory};
+use std::cell::OnceCell;
 use std::sync::Mutex;
 use std::{env::args, process};
 
-lazy_static! {
-    static ref INFO_FLAG: Mutex<Option<i32>> = Mutex::new(Some(0));
-}
+const INFO_FLAG: OnceCell<i32> = OnceCell::new();
 
 fn usage() {
     eprintln!(
@@ -29,7 +27,9 @@ fn main() {
     while let Some(arg) = args_iter.next() {
         match arg.as_str() {
             "-v" => version_flag = 1,
-            "-i" => *INFO_FLAG.lock().unwrap() = Some(1),
+            "-i" => {
+                INFO_FLAG.get_or_init(|| 1);
+            }
             "-f" => {
                 if let Some(file_name) = args_iter.next() {
                     custom_file = Some(file_name);
@@ -44,6 +44,9 @@ fn main() {
         }
     }
 
+    // If the user did not set -v set it to 0
+    INFO_FLAG.get_or_init(|| 0);
+
     if hostname.is_none() {
         usage();
         process::exit(1);
@@ -52,4 +55,6 @@ fn main() {
     if custom_directory.is_some() {
         geoip_setup_custom_directory(custom_directory.unwrap());
     }
+
+    _geo_ip_setup_dbfilename();
 }
