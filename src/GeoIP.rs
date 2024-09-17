6a876a9887a3de40122680fa7c4bef14c67b64f9
Triet Huynh <triet403@gmail.com>
2024-09-17 15:07:16 -0400

    separate

---
+++ src/GeoIP.rs
@@ -0,0 +1,185 @@
+use std::{
+    cell::OnceCell,
+    env,
+    ffi::{c_char, c_int, CStr, CString},
+};
+
+use crate::GeoIPCity_h::{
+    GeoIPDBTypes_GEOIP_ACCURACYRADIUS_EDITION, GeoIPDBTypes_GEOIP_ACCURACYRADIUS_EDITION_V6,
+    GeoIPDBTypes_GEOIP_ASNUM_EDITION, GeoIPDBTypes_GEOIP_ASNUM_EDITION_V6,
+    GeoIPDBTypes_GEOIP_CITYCONF_EDITION, GeoIPDBTypes_GEOIP_CITY_EDITION_REV0,
+    GeoIPDBTypes_GEOIP_CITY_EDITION_REV0_V6, GeoIPDBTypes_GEOIP_CITY_EDITION_REV1,
+    GeoIPDBTypes_GEOIP_CITY_EDITION_REV1_V6, GeoIPDBTypes_GEOIP_COUNTRYCONF_EDITION,
+    GeoIPDBTypes_GEOIP_COUNTRY_EDITION, GeoIPDBTypes_GEOIP_COUNTRY_EDITION_V6,
+    GeoIPDBTypes_GEOIP_DOMAIN_EDITION, GeoIPDBTypes_GEOIP_DOMAIN_EDITION_V6,
+    GeoIPDBTypes_GEOIP_ISP_EDITION, GeoIPDBTypes_GEOIP_ISP_EDITION_V6,
+    GeoIPDBTypes_GEOIP_LARGE_COUNTRY_EDITION, GeoIPDBTypes_GEOIP_LARGE_COUNTRY_EDITION_V6,
+    GeoIPDBTypes_GEOIP_LOCATIONA_EDITION, GeoIPDBTypes_GEOIP_LOCATIONA_EDITION_V6,
+    GeoIPDBTypes_GEOIP_NETSPEED_EDITION, GeoIPDBTypes_GEOIP_NETSPEED_EDITION_REV1,
+    GeoIPDBTypes_GEOIP_NETSPEED_EDITION_REV1_V6, GeoIPDBTypes_GEOIP_ORG_EDITION,
+    GeoIPDBTypes_GEOIP_ORG_EDITION_V6, GeoIPDBTypes_GEOIP_POSTALCONF_EDITION,
+    GeoIPDBTypes_GEOIP_PROXY_EDITION, GeoIPDBTypes_GEOIP_REGIONCONF_EDITION,
+    GeoIPDBTypes_GEOIP_REGION_EDITION_REV0, GeoIPDBTypes_GEOIP_REGION_EDITION_REV1,
+    GeoIPDBTypes_GEOIP_REGISTRAR_EDITION, GeoIPDBTypes_GEOIP_REGISTRAR_EDITION_V6,
+    GeoIPDBTypes_GEOIP_USERTYPE_EDITION, GeoIPDBTypes_GEOIP_USERTYPE_EDITION_V6,
+};
+
+const NUM_DB_TYPES: usize = 38 + 1;
+
+const GeoIPDBFileName: OnceCell<[String; NUM_DB_TYPES]> = OnceCell::new();
+const GeoIP_custom_directory: OnceCell<String> = OnceCell::new();
+
+const GeoIPDBDescription: [Option<&'static str>; NUM_DB_TYPES] = [
+    None,
+    Some("GeoIP Country Edition"),
+    Some("GeoIP City Edition, Rev 1"),
+    Some("GeoIP Region Edition, Rev 1"),
+    Some("GeoIP ISP Edition"),
+    Some("GeoIP Organization Edition"),
+    Some("GeoIP City Edition, Rev 0"),
+    Some("GeoIP Region Edition, Rev 0"),
+    Some("GeoIP Proxy Edition"),
+    Some("GeoIP ASNum Edition"),
+    Some("GeoIP Netspeed Edition"),
+    Some("GeoIP Domain Name Edition"),
+    Some("GeoIP Country V6 Edition"),
+    Some("GeoIP LocationID ASCII Edition"),
+    Some("GeoIP Accuracy Radius Edition"),
+    None,
+    None,
+    Some("GeoIP Large Country Edition"),
+    Some("GeoIP Large Country V6 Edition"),
+    None,
+    Some("GeoIP CCM Edition"),
+    Some("GeoIP ASNum V6 Edition"),
+    Some("GeoIP ISP V6 Edition"),
+    Some("GeoIP Organization V6 Edition"),
+    Some("GeoIP Domain Name V6 Edition"),
+    Some("GeoIP LocationID ASCII V6 Edition"),
+    Some("GeoIP Registrar Edition"),
+    Some("GeoIP Registrar V6 Edition"),
+    Some("GeoIP UserType Edition"),
+    Some("GeoIP UserType V6 Edition"),
+    Some("GeoIP City Edition V6, Rev 1"),
+    Some("GeoIP City Edition V6, Rev 0"),
+    Some("GeoIP Netspeed Edition, Rev 1"),
+    Some("GeoIP Netspeed Edition V6, Rev1"),
+    Some("GeoIP Country Confidence Edition"),
+    Some("GeoIP City Confidence Edition"),
+    Some("GeoIP Region Confidence Edition"),
+    Some("GeoIP Postal Confidence Edition"),
+    Some("GeoIP Accuracy Radius Edition V6"),
+];
+
+#[no_mangle]
+pub extern "C" fn GeoIP_setup_custom_directory(dir: *mut c_char) {
+    if dir.is_null() {
+        return;
+    }
+
+    let c_str = unsafe { CStr::from_ptr(dir) };
+
+    if let Ok(valid_str) = c_str.to_str() {
+        GeoIP_custom_directory.get_or_init(|| valid_str.to_string());
+    }
+}
+
+fn get_db_description(dbtype: i32) -> String {
+    if dbtype as usize >= NUM_DB_TYPES || dbtype < 0 {
+        return "Unknown".to_string();
+    }
+
+    GeoIPDBDescription
+        .get(dbtype as usize)
+        .unwrap()
+        .unwrap_or("Unknown")
+        .to_string()
+}
+
+fn _geo_ip_full_path_to(file_name: &str) -> String {
+    let path;
+    if let Some(custom_dir) = GeoIP_custom_directory.get() {
+        if custom_dir.chars().last().unwrap() != '/' {
+            path = format!("{}/{}", custom_dir, file_name);
+        } else {
+            path = format!("{}{}", custom_dir, file_name);
+        }
+    } else {
+        let geoip_data_dir = env::var("GEOIPDATADIR").unwrap_or_else(|_| "~/GeoIP".to_string());
+
+        path = format!("{}/{}", geoip_data_dir, file_name);
+    }
+    path
+}
+
+#[no_mangle]
+pub extern "C" fn _GeoIP_setup_dbfilename() {
+    GeoIPDBFileName.get_or_init(|| {
+        const EMPTY_STRING: String = String::new();
+        let mut result: [String; NUM_DB_TYPES] = [EMPTY_STRING; NUM_DB_TYPES];
+
+        result[GeoIPDBTypes_GEOIP_COUNTRY_EDITION as usize] = _geo_ip_full_path_to("GeoIP.dat");
+        result[GeoIPDBTypes_GEOIP_REGION_EDITION_REV0 as usize] =
+            _geo_ip_full_path_to("GeoIPRegion.dat");
+        result[GeoIPDBTypes_GEOIP_REGION_EDITION_REV1 as usize] =
+            _geo_ip_full_path_to("GeoIPRegion.dat");
+        result[GeoIPDBTypes_GEOIP_CITY_EDITION_REV0 as usize] =
+            _geo_ip_full_path_to("GeoIPCity.dat");
+        result[GeoIPDBTypes_GEOIP_CITY_EDITION_REV1 as usize] =
+            _geo_ip_full_path_to("GeoIPCity.dat");
+        result[GeoIPDBTypes_GEOIP_ISP_EDITION as usize] = _geo_ip_full_path_to("GeoIPISP.dat");
+        result[GeoIPDBTypes_GEOIP_ORG_EDITION as usize] = _geo_ip_full_path_to("GeoIPOrg.dat");
+        result[GeoIPDBTypes_GEOIP_PROXY_EDITION as usize] = _geo_ip_full_path_to("GeoIPProxy.dat");
+        result[GeoIPDBTypes_GEOIP_ASNUM_EDITION as usize] = _geo_ip_full_path_to("GeoIPASNum.dat");
+        result[GeoIPDBTypes_GEOIP_NETSPEED_EDITION as usize] =
+            _geo_ip_full_path_to("GeoIPNetSpeed.dat");
+        result[GeoIPDBTypes_GEOIP_DOMAIN_EDITION as usize] =
+            _geo_ip_full_path_to("GeoIPDomain.dat");
+        result[GeoIPDBTypes_GEOIP_COUNTRY_EDITION_V6 as usize] =
+            _geo_ip_full_path_to("GeoIPv6.dat");
+        result[GeoIPDBTypes_GEOIP_LOCATIONA_EDITION as usize] =
+            _geo_ip_full_path_to("GeoIPLocA.dat");
+        result[GeoIPDBTypes_GEOIP_ACCURACYRADIUS_EDITION as usize] =
+            _geo_ip_full_path_to("GeoIPDistance.dat");
+        result[GeoIPDBTypes_GEOIP_LARGE_COUNTRY_EDITION as usize] =
+            _geo_ip_full_path_to("GeoIP.dat");
+        result[GeoIPDBTypes_GEOIP_LARGE_COUNTRY_EDITION_V6 as usize] =
+            _geo_ip_full_path_to("GeoIPv6.dat");
+        result[GeoIPDBTypes_GEOIP_ASNUM_EDITION_V6 as usize] =
+            _geo_ip_full_path_to("GeoIPASNumv6.dat");
+        result[GeoIPDBTypes_GEOIP_ISP_EDITION_V6 as usize] = _geo_ip_full_path_to("GeoIPISPv6.dat");
+        result[GeoIPDBTypes_GEOIP_ORG_EDITION_V6 as usize] = _geo_ip_full_path_to("GeoIPOrgv6.dat");
+        result[GeoIPDBTypes_GEOIP_DOMAIN_EDITION_V6 as usize] =
+            _geo_ip_full_path_to("GeoIPDomainv6.dat");
+        result[GeoIPDBTypes_GEOIP_LOCATIONA_EDITION_V6 as usize] =
+            _geo_ip_full_path_to("GeoIPLocAv6.dat");
+        result[GeoIPDBTypes_GEOIP_REGISTRAR_EDITION as usize] =
+            _geo_ip_full_path_to("GeoIPRegistrar.dat");
+        result[GeoIPDBTypes_GEOIP_REGISTRAR_EDITION_V6 as usize] =
+            _geo_ip_full_path_to("GeoIPRegistrarv6,.dat");
+        result[GeoIPDBTypes_GEOIP_USERTYPE_EDITION as usize] =
+            _geo_ip_full_path_to("GeoIPUserType.dat");
+        result[GeoIPDBTypes_GEOIP_USERTYPE_EDITION_V6 as usize] =
+            _geo_ip_full_path_to("GeoIPUserTypev6.dat");
+        result[GeoIPDBTypes_GEOIP_CITY_EDITION_REV0_V6 as usize] =
+            _geo_ip_full_path_to("GeoIPCityv6.dat");
+        result[GeoIPDBTypes_GEOIP_CITY_EDITION_REV1_V6 as usize] =
+            _geo_ip_full_path_to("GeoIPCityv6.dat");
+        result[GeoIPDBTypes_GEOIP_NETSPEED_EDITION_REV1 as usize] =
+            _geo_ip_full_path_to("GeoIPNetSpeedCell.dat");
+        result[GeoIPDBTypes_GEOIP_NETSPEED_EDITION_REV1_V6 as usize] =
+            _geo_ip_full_path_to("GeoIPNetSpeedCellv6.dat");
+        result[GeoIPDBTypes_GEOIP_COUNTRYCONF_EDITION as usize] =
+            _geo_ip_full_path_to("GeoIPCountryConf.dat");
+        result[GeoIPDBTypes_GEOIP_CITYCONF_EDITION as usize] =
+            _geo_ip_full_path_to("GeoIPCityConf.dat");
+        result[GeoIPDBTypes_GEOIP_REGIONCONF_EDITION as usize] =
+            _geo_ip_full_path_to("GeoIPRegionConf.dat");
+        result[GeoIPDBTypes_GEOIP_POSTALCONF_EDITION as usize] =
+            _geo_ip_full_path_to("GeoIPPostalConf.dat");
+        result[GeoIPDBTypes_GEOIP_ACCURACYRADIUS_EDITION_V6 as usize] =
+            _geo_ip_full_path_to("GeoIPDistancev6.dat");
+
+        result
+    });
+}
