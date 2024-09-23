21134c2dceac08a731feff0f7de7be73dfaf0f20
HuMiTriet <triet403@gmail.com>
2024-09-23 16:12:09 -0400

    working MVP with hack two fields at the end of the struct

--- c_code/geoiplookup.c
+++ c_code/geoiplookup.c
@@ -1,15 +1,16 @@
 #include "GeoIP.h"
-#include "GeoIPCity.h"
-#include "GeoIP_internal.h"
 #include "helper.h"
 #include <stdio.h>
 
 int main() {
-  GeoIP *gi = GeoIP_open("/home/pj/coding/geoip-api-c/data/GeoIP.dat",
+  GeoIP *gi = GeoIP_open("/home/triet/coding/rust_libgeoip/data/GeoIP.dat",
                          GEOIP_STANDARD | GEOIP_SILENCE);
-  printf("C code: File last mtime check is: %p \n", &gi->last_mtime_check);
+  printf("C code: File mtime check is: %ld at addr: %p \n", gi->mtime,
+         &gi->mtime);
 
   int test = get_fileno(gi);
+
+  printf("fileno helper from rust result: %d \n", test);
 
   return 0;
 }
